8ea4650516d29c6aa0481cffb12b5fd8930ab5e4
sushruth.chandrashek <sushruth.chandrashek@servicenow.com>
2018-01-04 00:38:04 -0800

    updates

--- Lab4/idea_sidebar_clientscript.js
+++ Lab4/idea_sidebar_clientscript.js
@@ -8,15 +8,16 @@ function ideaSidebarCtrl($location, $scope, $rootScope, $window) {
 
     //Submit an idea button to take users to a new page with record producer
     c.submitIdea = function() {
-        $location.search({
-            id: 'idea_create',
-            sys_id: '97948f5bdb47070000e4f5861d96190e'
-        });
-        //$window.location.href = "/ideas?id=idea_create&table=idea&sys_id=-1";
+        // $location.search({
+        //     id: 'idea_create',
+        //     sys_id: '97948f5bdb47070000e4f5861d96190e'
+        // });
+        //$window.location.href = "/ideas?id=idea_create&sys_id=97948f5bdb47070000e4f5861d96190e";
     };
 
     //Broadcast event based on filter links. This event is caught in Idea List widget
     c.filterClicked = function(type) {
+        console.log('broadcast fired and type is ' + type);
         c.selectedFilter = type;
         $rootScope.$broadcast('filterClicked', type);
     }
